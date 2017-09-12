76ae14a063750714c4d693e6a183c6eccd999bca
Jordan Shurmer <jshurmer@scrippsnetworks.com>
2017-09-12 16:01:34 -0400

    Add ability to toggle between different heatmap value attributes

--- results.js
+++ results.js
@@ -1,3 +1,6 @@
+//Some globals
+let heatmap, allData;
+
 //Improve mobile map viewing
 if (navigator.userAgent.indexOf('iPhone') !== -1
     || navigator.userAgent.indexOf('Android') !== -1) {
@@ -12,169 +15,6 @@ if (navigator.userAgent.indexOf('iPhone') !== -1
  */
 function initMap() {
   //create a greyed-out map
-  let grayMap = new google.maps.StyledMapType(
-      [
-        {
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#f5f5f5"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.land_parcel",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#bdbdbd"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.park",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "road",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#ffffff"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#757575"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#dadada"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#616161"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.line",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#e5e5e5"
-            }
-          ]
-        },
-        {
-          "featureType": "transit.station",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#eeeeee"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#c9c9c9"
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#9e9e9e"
-            }
-          ]
-        }
-      ],
-      {name: 'Gray Map'}
-  );
 
   //create the map
   let map = new google.maps.Map(document.getElementById('map'),
@@ -188,9 +28,8 @@ function initMap() {
       });
 
   //Associate the gray map with the MapTypeId and set it to display.
-  map.mapTypes.set('gray_map', grayMap);
+  map.mapTypes.set('gray_map', new google.maps.StyledMapType(MAPSTYLES.gray, {name: 'Gray Map'}));
   map.setMapTypeId('gray_map');
-
 
   //AWS Connection
   let dynamodb = new AWS.DynamoDB({
@@ -200,27 +39,206 @@ function initMap() {
     logger: console
   });
 
-
   //load data from dynamo
-  let allAddrs = [];
   dynamodb.scan({TableName: "ViewerData"}, (error, data) => {
+
     if (error !== null) {
       //TODO: display this on page in a useful way somehow
       console.error("DynamoDB Error", error);
+
     } else {
       console.info("Successful DynamoDB request");
       console.debug(data);
 
-      data.Items.forEach(item => allAddrs.push({
-        location: new google.maps.LatLng(item.lat.S, item.lng.S),
-        weight: item.appraised.S //TODO: create way to change the value used
-      }));
+      //Save the data
+      allData = data.Items;
 
-      let heatmap = new google.maps.visualization.HeatmapLayer({
-        data: allAddrs,
+      //Build the Heatmap layer
+      heatmap = new google.maps.visualization.HeatmapLayer({
         map: map
       });
+
+      //set the heatmap data
+      setHeatmapData('assessed');
     }
   });
 
 }
+
+function setHeatmapData(attributeName) {
+  let heatMapData = [];
+  //Add each address to the list of addresses, with a weight coming from the attributeName passed in
+  allData.forEach(item => {
+    console.debug("Adding item to heatmap data", item);
+    heatMapData.push({
+      location: new google.maps.LatLng(item.lat.S, item.lng.S),
+      weight: item[attributeName].S
+    })
+  });
+
+  console.debug("Setting heatmap data", heatMapData);
+  heatmap.setData(heatMapData);
+}
+
+MAPSTYLES = {
+  gray: [
+    {
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#f5f5f5"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative.land_parcel",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#bdbdbd"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "poi.park",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#757575"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#dadada"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#616161"
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.line",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#e5e5e5"
+        }
+      ]
+    },
+    {
+      "featureType": "transit.station",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#c9c9c9"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#9e9e9e"
+        }
+      ]
+    }
+  ]
+};
